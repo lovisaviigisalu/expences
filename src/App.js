63ed22efa68849dd86aa30ed67138a163875c9da
Lovisa Viigisalu <l.viigisalu@gmail.com>
2023-02-08 23:30:47 +0200

    Create first custom component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import '/ExpenseItem.css'
+import './ExpenseItem.css'
 
 function ExpenseItem(){
     return(
-        <div className='expense-item'>
+        <div className={'expense-item'}>
             <div>Data</div>
-            <div className='expense-item__description.'>
+            <div className={'expense-item__description'}>
                 <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <div className={'expense-item__price'}>Amount</div>
             </div>
 
         </div>
