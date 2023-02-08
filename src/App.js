842bcf25f7e0d015b365ee6cd3236b83b1e882f7
Lovisa Viigisalu <l.viigisalu@gmail.com>
2023-02-08 21:42:47 +0200

    Create first custom component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem></ExpenseItem>
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
